b097cac33e7e5fe3898b507c6247cb431d4ad00f
ishanrijal <ishanrijal6@gmail.com>
2024-09-24 01:04:40 +0545

    view password btn

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -82,5 +82,34 @@
         })();
     </script>
     <!--End of Tawk.to Script-->
+    <script>
+        document.getElementById('togglePassword').addEventListener('click', function() {
+            const passwordInput = document.getElementById('password');
+            const eyeIcon = document.getElementById('eyeIcon');
+    
+            // Toggle the type attribute
+            if (passwordInput.type === 'password' ) {
+                passwordInput.type = 'text'; // Show password
+                eyeIcon.innerHTML = '<path fill="currentColor" d="M572.52 241.4c2.55 4.29 2.55 9.52 0 13.81C512.95 361.18 407.77 448 288 448S63.05 361.18 3.48 255.21a10.04 10.04 0 010-13.81C63.05 150.82 168.23 64 288 64s224.95 86.82 284.52 192.4zM288 128c-44.18 0-80 35.82-80 80s35.82 80 80 80 80-35.82 80-80-35.82-80-80-80zm0 224c-79.4 0-158.26-51.34-222.94-144C129.74 211.34 208.6 160 288 160s158.26 51.34 222.94 144C446.26 300.66 367.4 352 288 352z"/>';
+            } else {
+                passwordInput.type = 'password'; // Hide password
+                eyeIcon.innerHTML = '<path fill="currentColor" d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144 144 0 0 1-26 2.61m313.82 58.1l-110.55-85.44a331.3 331.3 0 0 0 81.25-102.07a32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45"/>';
+            }            
+        });
+        document.getElementById('togglePasswordConfirmation').addEventListener('click', function() {
+            const passwordConfirmationInput = document.getElementById('password_confirmation');
+            const eyeIconConfirmation = document.getElementById('eyeIconConfirmation');
+            // for confirm passwor
+            if (passwordConfirmationInput.type === 'password' ) {
+                passwordConfirmationInput.type = 'text'; // Show password
+                eyeIconConfirmation.innerHTML = '<path fill="currentColor" d="M572.52 241.4c2.55 4.29 2.55 9.52 0 13.81C512.95 361.18 407.77 448 288 448S63.05 361.18 3.48 255.21a10.04 10.04 0 010-13.81C63.05 150.82 168.23 64 288 64s224.95 86.82 284.52 192.4zM288 128c-44.18 0-80 35.82-80 80s35.82 80 80 80 80-35.82 80-80-35.82-80-80-80zm0 224c-79.4 0-158.26-51.34-222.94-144C129.74 211.34 208.6 160 288 160s158.26 51.34 222.94 144C446.26 300.66 367.4 352 288 352z"/>';
+            } else {
+                passwordConfirmationInput.type = 'password'; // Hide password
+                eyeIconConfirmation.innerHTML = '<path fill="currentColor" d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144 144 0 0 1-26 2.61m313.82 58.1l-110.55-85.44a331.3 331.3 0 0 0 81.25-102.07a32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45"/>';
+            }
+            
+        });
+    </script>
+    
 </body>
 </html>
